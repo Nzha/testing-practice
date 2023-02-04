8da82aaadef701580531c99d2a05d2061d0ec399
Adrien <adrien.biz@gmail.com>
2023-02-04 14:40:37 +0400

    Clean up code and check if array contains numbers only

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,24 +1,22 @@
+const onlyNumbers = (arr) => {
+  return arr.every((el) => {
+    return typeof el === 'number';
+  });
+};
+
 const analyzeArray = (arr) => {
+  if (!onlyNumbers(arr)) return 'Array must include only numbers';
   let result = {};
   let average = arr.reduce((a, b) => a + b, 0) / arr.length;
   result.average = Math.round(average * 10) / 10;
-
-  let min = arr[0];
-  let max = arr[0];
-  arr.forEach((el) => {
-    if (el < min) min = el;
-    if (el > max) max = el;
-  });
-  result.min = min;
-  result.max = max;
-
+  result.min = Math.min(...arr);
+  result.max = Math.max(...arr);
   result.length = arr.length;
-
   return result;
 };
 
 export default analyzeArray;
 
-const testArray = analyzeArray([1, 8, 3, 4, 2, 6]);
+const testArray = analyzeArray([1, 8, 3, 4, 2, '6']);
 
 console.log(testArray);
--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -10,10 +10,16 @@ test('[1, 4, 8] is {average: 4.3, min: 1, max: 8, length: 3}', () => {
 });
 
 test('[1,8,3,4,2,6] is {average: 4, min: 1, max: 8, length: 6}', () => {
-  expect(analyzeArray([1,8,3,4,2,6])).toEqual({
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
     average: 4,
     min: 1,
     max: 8,
-    length: 6
+    length: 6,
   });
+});
+
+test('[1,8,3,4,2,"6"] returns error message', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, '6'])).toBe(
+    'Array must include only numbers'
+  );
 });
